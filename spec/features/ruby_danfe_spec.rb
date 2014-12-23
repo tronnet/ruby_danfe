cdaacb1261572a415144bc84dde0444a5ab0196a
Pedro Henrique <pedrohenriquerls@gmail.com>
2014-12-23 13:54:52 -0200

    alinhamento de versões ecorreções para testes unitários

--- spec/features/ruby_danfe_spec.rb
+++ spec/features/ruby_danfe_spec.rb
@@ -4,10 +4,17 @@ describe "RubyDanfe generated pdf files" do
   let(:base_dir) { "./spec/fixtures/"}
   let(:output_pdf) { "#{base_dir}output.pdf" }
 
-  before { File.delete(output_pdf) if File.exist?(output_pdf) }
+  before(:each) do
+    File.delete(output_pdf) if File.exist?(output_pdf)
+  end
+
+  after(:all) do
+    pdf = "./spec/fixtures/output.pdf"
+    File.delete(pdf) if File.exist?(pdf)
+  end
 
   it "renders a basic NF-e with namespace" do
-    expect(File.exist?(output_pdf)).to be_false
+    expect(File.exist?(output_pdf)).to be false
 
     RubyDanfe.generate(output_pdf, "#{base_dir}nfe_with_ns.xml")
 
@@ -15,7 +22,7 @@ describe "RubyDanfe generated pdf files" do
   end
 
   it "renders another basic NF-e without namespace" do
-    expect(File.exist?(output_pdf)).to be_false
+    expect(File.exist?(output_pdf)).to be false
 
     RubyDanfe.generate(output_pdf, "#{base_dir}nfe_without_ns.xml")
 
@@ -23,7 +30,7 @@ describe "RubyDanfe generated pdf files" do
   end
 
   it "renders a NF-e having FCI in its items" do
-    expect(File.exist?(output_pdf)).to be_false
+    expect(File.exist?(output_pdf)).to be false
 
     RubyDanfe.generate(output_pdf, "#{base_dir}nfe_with_fci.xml")
 
@@ -31,7 +38,7 @@ describe "RubyDanfe generated pdf files" do
   end
 
   it "renders a NF-e of Simples Nacional using CSOSN" do
-    expect(File.exist?(output_pdf)).to be_false
+    expect(File.exist?(output_pdf)).to be false
 
     RubyDanfe.generate(output_pdf, "#{base_dir}nfe_simples_nacional.xml")
 
@@ -39,7 +46,7 @@ describe "RubyDanfe generated pdf files" do
   end
 
   it "renders a basic CT-e" do
-    expect(File.exist?(output_pdf)).to be_false
+    expect(File.exist?(output_pdf)).to be false
 
     RubyDanfe.generate(output_pdf, "#{base_dir}cte.xml", :dacte)
 
